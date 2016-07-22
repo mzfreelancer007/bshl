96659be3b884c39c66ab0f6fa2443313f84aefac
ashek <mafia.mithun@gmail.com>
2016-07-23 01:13:42 +0600

    add a middle wire for allow multi domain request access

--- server.js
+++ server.js
@@ -16,6 +16,20 @@ app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+//Express application to allow multiple origins
+app.use(function(req, res, next) {
+  var allowedOrigins = ['http://localhost:5000', 'http:test-demom.herokuapp.com'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 //*------------------------------api--------------------------------*
 
 app.get('/api/alltodo',function(req,res){
